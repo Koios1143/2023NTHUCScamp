a8519d3b80a0fcc22607755a539e2048cb755c89
llshang <lishang0225@gmail.com>
2023-09-09 13:11:26 +0800

    add project template

---
+++ project/main.py
@@ -0,0 +1,35 @@
+import discord
+import requests
+from discord.ext import commands
+
+with open('token.txt', 'r') as f:
+    ACCESS_TOKEN = f.readline()
+
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+
+# TODO: Add json link
+# response = requests.get('<link>').json()
+
+# Example
+response = requests.get('https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/OPENDATA/open_course_data.json').json()
+
+@bot.event
+async def on_ready():
+    try:
+        sync = await bot.tree.sync()
+        print(f'We have logged in as {bot.user}')
+    except Exception as e:
+        print(e)
+
+# TODO: Add your command
+# @bot.tree.command(name='<指令名稱>', description='<指令敘述：說明指令功能>')
+# async def <指令名稱>(interaction: discord.Interaction, msg: str):
+#     指令操作...
+#     await interaction.response.send_message("<傳送訊息>")
+
+# Example
+@bot.tree.command(name='echo', description='傳入一個參數 msg，並回傳 msg 的內容')
+async def echo(interaction: discord.Interaction, msg: str):
+    await interaction.response.send_message(msg)
+
+bot.run(ACCESS_TOKEN)
